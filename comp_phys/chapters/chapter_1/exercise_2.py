cb0eee79bc96ef44dbb18a8c7ee4f4aa2fe1c735
Jamison Lahman <lahmanja@protonmail.com>
2020-03-08 01:04:20 -0800

    polish ex 2

--- comp_phys/chapters/chapter_1/exercise_2.py
+++ comp_phys/chapters/chapter_1/exercise_2.py
@@ -11,8 +11,9 @@ by Steven E. Koonin and Dawn C. Meredith
 Solution by Jamison Lahman, June 11, 2018
 """
 import math
+import os
 
-from chapters.chapter_1 import quadrature
+from comp_phys.library import quadrature
 
 
 def my_function(x):
@@ -25,26 +26,31 @@ def my_function(x):
 
 
 def main():
+    """Executes exercise 1.2"""
+
+    file_directory = os.path.dirname(os.path.realpath(__file__))
+    output_filepath = os.path.join(file_directory, 'output/exercise_2.txt')
+
     a = 0.0                                         # lower bound
     b = 1.0                                         # upper bound
-    N = [4, 8, 16, 32, 64, 128]                     # number of lattices
+    N_list = [4, 8, 16, 32, 64, 128]                # number of lattices
 
     # The exact solution is the antiderivative (exp.math(x) in this case)
     # evaluated at the upper bound minus the value at the lower bound.
     exact = math.exp(b) - math.exp(a)
 
-    with open('exercise1_2.txt', 'w+') as f:
-        for i in range(len(N)):
+    with open(output_filepath, 'w+') as out_file:
+        for N in N_list:
             # Step size is the range of the area of integration divided by number of lattices
-            h = (b - a) / N[i]
+            h = (b - a) / N
 
-            error1 = quadrature.trapezoidal(my_function, a, h, N[i]) - exact
-            error2 = quadrature.simpsons(my_function, a, h, N[i]) - exact
-            error3 = quadrature.booles(my_function, a, h, N[i]) - exact
+            error1 = quadrature.trapezoidal(my_function, a, h, N) - exact
+            error2 = quadrature.simpsons(my_function, a, h, N) - exact
+            error3 = quadrature.booles(my_function, a, h, N) - exact
 
             # Outputs in a format compatible with LaTex tabular :)
-            fout.write('{0:.5f} & {1:.6f} & {2:.6f} & {3:.6f} & {4:.6f} \n'.format(
-                N[i], h, error1, error2, error3))
+            out_file.write('{0:.5f} & {1:.6f} & {2:.6f} & {3:.6f} & {4:.6f}\n'.format(
+                N, h, error1, error2, error3))
 
 
 if __name__ == '__mian__':
