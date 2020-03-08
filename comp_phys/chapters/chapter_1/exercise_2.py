ae7f3e085c7f9b1366c009d0d1ba805c5447d196
Jamison Lahman <lahmanja@protonmail.com>
2020-03-08 00:39:37 -0800

    lint

--- comp_phys/chapters/chapter_1/exercise_2.py
+++ comp_phys/chapters/chapter_1/exercise_2.py
@@ -14,6 +14,7 @@ import math
 
 from chapters.chapter_1 import quadrature
 
+
 def my_function(x):
     """
     Function to be evaluated.
@@ -22,26 +23,29 @@ def my_function(x):
     """
     return math.exp(x)
 
+
 def main():
     a = 0.0                                         # lower bound
     b = 1.0                                         # upper bound
     N = [4, 8, 16, 32, 64, 128]                     # number of lattices
 
-    #The exact solution is the antiderivative (exp.math(x) in this case)
-    #evaluated at the upper bound minus the value at the lower bound.
+    # The exact solution is the antiderivative (exp.math(x) in this case)
+    # evaluated at the upper bound minus the value at the lower bound.
     exact = math.exp(b) - math.exp(a)
 
-    with open('exercise1_2.txt','w+') as f:
+    with open('exercise1_2.txt', 'w+') as f:
         for i in range(len(N)):
-            #Step size is the range of the area of integration divided by number of lattices
+            # Step size is the range of the area of integration divided by number of lattices
             h = (b - a) / N[i]
 
-            error1 = quadrature.trapezoidal(my_function,a, h, N[i]) - exact
+            error1 = quadrature.trapezoidal(my_function, a, h, N[i]) - exact
             error2 = quadrature.simpsons(my_function, a, h, N[i]) - exact
             error3 = quadrature.booles(my_function, a, h, N[i]) - exact
 
-            #Outputs in a format compatible with LaTex tabular :)
-            fout.write('{0:.5f} & {1:.6f} & {2:.6f} & {3:.6f} & {4:.6f} \n'.format(N[i],h,error1,error2,error3))
+            # Outputs in a format compatible with LaTex tabular :)
+            fout.write('{0:.5f} & {1:.6f} & {2:.6f} & {3:.6f} & {4:.6f} \n'.format(
+                N[i], h, error1, error2, error3))
+
 
 if __name__ == '__mian__':
     main()
