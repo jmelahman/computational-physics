e3768aae020f6ec9b34d2d5791c33be10a7c31bc
Jamison Lahman <lahmanja@protonmail.com>
2020-10-11 13:38:08 -0700

    launcher

--- launcher.py
+++ launcher.py
@@ -7,24 +7,28 @@ def run_module(name):
     module.main()
 
 def main(chapter, exercise, example, project):
-    chapters_path = 'chapter'
+    chapters_path = 'phys.chapter_{}'.format(chapter)
     if exercise:
-        module_name = '{}.{}.exercise.{}'.format(chapters_path, chapter, exercise)
+        module_name = '{}.exercise.{}'.format(chapters_path, exercise)
         run_module(module_name)
     if example:
-        module_name = '{}.{}.example'.format(chapters_path, chapter)
+        module_name = '{}.example'.format(chapters_path)
         run_module(module_name)
     if project:
-        module_name = '{}.{}.project'.format(chapters_path, chapter)
+        module_name = '{}.project'.format(chapters_path)
         run_module(module_name)
 
-if __name__ == '__main__':
+def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('chapter', type=int, help='select a chapter')
-    parser.add_argument('exercise', nargs='?', type=int, help='select an exercise \
+    parser.add_argument('chapter', help='select a chapter')
+    parser.add_argument('exercise', nargs='?', help='select an exercise \
         (optional if selecting the example or project)')
     parser.add_argument('-e', '--example', action='store_true', help='select the example')
     parser.add_argument('-p', '--project', action='store_true', help='select the project')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
 
     main(args.chapter, args.exercise, args.example, args.project)
